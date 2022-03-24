a8914ecddc30bf4291f019f7ac06d86307344c76
Tresor <tresoralain35@gmail.com>
2022-03-24 17:02:03 +0200

    updates

--- dashboard/javascript.js
+++ dashboard/javascript.js
@@ -5,3 +5,11 @@ isOn?div[0].style.display="none": div[0].style.display="flex";
 isOn=!isOn;
 }
 document.getElementById("showMenu").addEventListener('click', showMenu);
+
+let isUserProfileOn=false;
+const showUserProfile=()=>{
+    const div=document.querySelectorAll(".user-profile");
+    isUserProfileOn?div[0].style.display="none": div[0].style.display="block";
+isUserProfileOn=!isUserProfileOn;
+}
+document.getElementById("userDiv").addEventListener('click', showUserProfile);
--- javascript.js
+++ javascript.js
@@ -5,3 +5,34 @@ isOn?div[0].style.display="none": div[0].style.display="flex";
 isOn=!isOn;
 }
 document.getElementById("showMenu").addEventListener('click', showMenu);
+
+const switchContent=id=>{
+    const about=document.getElementById("aboutBtn");
+    const aboutContent=document.querySelector(".show-about");
+    const skills=document.getElementById("skillsBtn");
+    const skillsContent=document.querySelector(".show-skills");
+    const experience=document.getElementById("experience");
+    if(id=="aboutBtn")
+    {
+        skillsContent.style.display="none";
+        skills.className="";
+        
+        // experience.style.display="none";
+        // experience.className="";
+
+        aboutContent.style.display="block";
+        about.className="active";
+    }
+    if(id=="skillsBtn")
+    {
+        aboutContent.style.display="none";
+        about.className="";
+
+        skillsContent.style.display="block";
+        skills.className="active";
+        
+        // experience.style.display="none";
+        // experience.className="";
+
+    }
+}
