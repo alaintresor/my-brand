3eeac871c34abea639d06ddfa7ecce6d0848994e
Tresor <tresoralain35@gmail.com>
2022-04-04 16:07:25 +0200

    updates

--- dashboard/javascript.js
+++ dashboard/javascript.js
@@ -16,7 +16,20 @@ document.getElementById("userDiv").addEventListener('click', showUserProfile);
 
 ///////////////////////////////////
 
+const isAdminLogedIn=()=>{
+    const loginedUser = JSON.parse(localStorage.getItem("loginedUser")) || [];
+    if (loginedUser.role != "admin") {
+      location.href='../login.html'
+    }
+}
+isAdminLogedIn()
 
+const logout = () => {
+    localStorage.removeItem("loginedUser");
+    location.href='../index.html'
+  };
+
+document.getElementById("logoutBtn").addEventListener('click',logout)
 
 
 
